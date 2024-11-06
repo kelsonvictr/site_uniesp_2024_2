d7042535b3f226d8750dec8a93d7bd5fa9070cb2
kelsonalmeida <kelson.victr@gmail.com>
2024-11-06 20:58:15 -0300

    aula01:  App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { Container, CssBaseline, Box } from '@mui/material'
 import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import BannerAd from './components/BannerAd'
+import Inicial from './pages/Inicial'
 
 const App = () => {
   return (
@@ -10,6 +12,16 @@ const App = () => {
       <Container maxWidth='lg' sx={{ backgroundColor: '#f7f7f7', minHeight: '100vh', padding: '16px', borderRadius: '8px'  }}>
         <Box display="flex" alignItems="center" justifyContent="space-between" p={2} sx={{ backgroundColor: '#fff', borderRadius: '8px', boxShadow: 1 }}>
           <Navbar />
+        </Box>
+
+        <BannerAd />
+
+        <Box mt={4}>
+
+          <Routes>
+            <Route path="/" element={<Inicial />} />
+          </Routes>
+
         </Box>
 
       </Container>
